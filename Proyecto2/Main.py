b99e42b2aa77d09cb35e9264fb265fa4146e05cb
Jsfgefge-AngelMs <Josemarroquin.jm52@gmail.com>
2020-10-21 23:25:01 -0600

    Inicio de la interfaz grafica

--- Proyecto2/Main.py
+++ Proyecto2/Main.py
@@ -2,83 +2,16 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-import matplotlib.pyplot as plt #Para el grid
-from matplotlib import colors #Para los colores de las celulas
-import matplotlib.animation as animation
-import numpy as np #Tengo entendido que para el uso de matrices
+class ventana(Gtk.Window):
+	def __init__(self):
+		Gtk.Window.__init__(self, title='El juego de la vida')
+		self.interfaz()
+		
 
-ON = 1
-OFF = 0
-pause = False
-def update(frame, img, grid, N):
-	newGrid = grid.copy()
-	for i in range(N):
-		for j in range(N):
-			total = int((grid[i, (j - 1) % N] + grid[i, (j + 1) % N] +
-						 grid[(i - 1) % N, j] + grid[(i + 1) % N, j] +
-						 grid[(i - 1) % N, (j - 1) % N] + grid[(i - 1) % N, (j + 1) % N] +
-						 grid[(i + 1) % N, (j - 1) % N] + grid[(i + 1) % N, (j + 1) % N]))
+	def interfaz(self):
+		grid = Gtk.Grid()
+		self.add(grid)
 
-
-			#Reglas del juego
-			if grid[i, j] == OFF and total == 3:
-				newGrid[i, j] = ON
-			elif grid[i,j] == ON and (total < 2 or total > 3):
-				newGrid[i, j] = OFF
-
-		# update data
-	img.set_data(newGrid)
-	grid[:] = newGrid[:]
-	return img,
-
-def main():
-	N = 10
-	updateInterval = 10
-	data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-			[1, 1, 0, 0, 0, 1, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-	grid = np.array(data)
-	# Basicamente para colorear
-	cmap = colors.ListedColormap(['blue','red'])  # Para seleccionar que color queremos
-	bounds = [0, 1, 2]  # Para verificar que valores tienen que ser rojo y cuales azules (0=azul,1=rojo)
-	norm = colors.BoundaryNorm(bounds, cmap.N)  # Para colorear el grid
-	#Generacion del Grid y su animacion
-	fig, ax = plt.subplots()
-	img = ax.imshow(grid, cmap=cmap, norm=norm)
-	ani = animation.FuncAnimation(fig, update, fargs=(img, grid, N,),
-								  frames=20,
-								  interval=updateInterval,
-								  save_count=50,
-								  repeat=True)
-
-	anim_running = True
-	def onClick(event):
-		nonlocal anim_running
-		if anim_running:
-			ani.event_source.stop()
-			anim_running = False
-		else:
-			ani.event_source.start()
-			anim_running = True
-
-	fig.canvas.mpl_connect('button_press_event', onClick)
-
-	ax.set_xticks(np.arange(0.48, 10, 1))
-	ax.set_yticks(np.arange(0.48, 10, 1))
-
-	ax.set_yticklabels([])
-	ax.set_xticklabels([])
-	plt.grid()
-	plt.tight_layout()
-	plt.show()
-
-
-
-main()
+win = ventana()
+win.show_all()
+Gtk.main()
---
+++ Proyecto2/funcionBotones.py
@@ -0,0 +1,84 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+import matplotlib.pyplot as plt #Para el grid
+from matplotlib import colors #Para los colores de las celulas
+import matplotlib.animation as animation
+import numpy as np #Tengo entendido que para el uso de matrices
+
+ON = 1
+OFF = 0
+pause = False
+def reglas(frame, img, tabla, N):
+	nTabla = tabla.copy()
+	for i in range(N):
+		for j in range(N):
+			total = int((tabla[i, (j - 1) % N] + tabla[i, (j + 1) % N] +
+						 tabla[(i - 1) % N, j] + tabla[(i + 1) % N, j] +
+						 tabla[(i - 1) % N, (j - 1) % N] + tabla[(i - 1) % N, (j + 1) % N] +
+						 tabla[(i + 1) % N, (j - 1) % N] + tabla[(i + 1) % N, (j + 1) % N]))
+
+
+			#Reglas del juego
+			if tabla[i, j] == 0 and total == 3:
+				nTabla[i, j] = 1
+			elif tabla[i,j] == 1 and (total < 2 or total > 3):
+				nTabla[i, j] = 0
+
+		# update data
+	img.set_data(nTabla)
+	tabla[:] = nTabla[:]
+	return img,
+
+def main():
+	N = 10
+	updateInterval = 10
+	data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+			[1, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+			[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+			[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+	grid = np.array(data)
+	# Basicamente para colorear
+	cmap = colors.ListedColormap(['blue','red'])  # Para seleccionar que color queremos
+	bounds = [0, 1, 2]  # Para verificar que valores tienen que ser rojo y cuales azules (0=azul,1=rojo)
+	norm = colors.BoundaryNorm(bounds, cmap.N)  # Para colorear el grid
+	#Generacion del Grid y su animacion
+	fig, ax = plt.subplots()
+	img = ax.imshow(grid, cmap=cmap, norm=norm)
+	ani = animation.FuncAnimation(fig, reglas, fargs=(img, grid, N,),
+								  frames=20,
+								  interval=updateInterval,
+								  save_count=50,
+								  repeat=True)
+
+	anim_running = True
+	def onClick(event):
+		nonlocal anim_running
+		if anim_running:
+			ani.event_source.stop()
+			anim_running = False
+		else:
+			ani.event_source.start()
+			anim_running = True
+
+	fig.canvas.mpl_connect('button_press_event', onClick)
+
+	ax.set_xticks(np.arange(0.48, 10, 1))
+	ax.set_yticks(np.arange(0.48, 10, 1))
+
+	ax.set_yticklabels([])
+	ax.set_xticklabels([])
+	plt.grid()
+	plt.tight_layout()
+	plt.show()
+
+
+
+main()
