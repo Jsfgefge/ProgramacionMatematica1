37d059b9beac6f7c78cc9b1a76b620688fe4a5ca
Jsfgefge-AngelMs <Josemarroquin.jm52@gmail.com>
2020-10-30 21:02:08 -0600

    Adicion de varias cosas 30Oct 21:02

--- Proyecto2/Main.py
+++ Proyecto2/Main.py
@@ -1,6 +1,8 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
+import time
+import random
 
 import matplotlib.pyplot as plt #Para el grid
 import matplotlib as mlp
@@ -11,6 +13,8 @@ import numpy as np #Tengo entendido que para el uso de matrices
 from matplotlib.backends.backend_gtk3agg import (
     FigureCanvasGTK3Agg as FigureCanvas)
 from matplotlib.figure import Figure
+
+import os
 
 UI_INFO = """
 <ui>
@@ -39,67 +43,105 @@ UI_INFO = """
 class ventana(Gtk.Window):
 	def __init__(self):
 		Gtk.Window.__init__(self, title='El juego de la vida')
-
+		#Parametro de inicio utiles
+		self.anim_running = False
 		self.fronteras = 'Normal'
 		self.intervaloActu = 100
 		self.dimensionMatriz = 10
 		self.conteo = 1
-
 		self.ruta = ''
 
+		#Para iniciar el programa con un grid 'muerto'
+		self.grid = np.zeros((self.dimensionMatriz,self.dimensionMatriz), dtype=int)
+
+		#Inicializacion de lo necesario para la animacion
+		self.figure = mlp.figure.Figure()
+		self.canvas = FigureCanvas(self.figure)
+
+		#Tamaño de la ventana
 		self.set_default_size(700,00)
-
-		action = Gtk.ActionGroup(name='action')
-
-		self.menuArchivo(action)
-		self.menuConfig(action)
-		self.menuAyuda(action)
-
+		#Para el barmenu
+		self.action = Gtk.ActionGroup(name='action')
+		self.menuArchivo(self.action)
+		self.menuConfig(self.action)
+		self.menuAyuda(self.action)
 		uimanager = self.create_ui_manager()
-		uimanager.insert_action_group(action)
-
+		uimanager.insert_action_group(self.action)
 		menubar = uimanager.get_widget('/menuBar')
 
-		grid = Gtk.Grid()
-		grid.set_row_spacing(5)
-		grid.set_column_spacing(5)
-		grid.attach(menubar, 0, 0, 10, 1)
-		self.add(grid)
+		#Grid donde estara toda la interfaz
+		container = Gtk.Grid()
+		container.set_row_spacing(5)
+		container.set_column_spacing(5)
+		container.attach(menubar, 0, 0, 10, 1)
+		self.add(container)
 
-		sw = Gtk.ScrolledWindow()
-		grid.attach(sw, 0, 1, 60, 90)
-		sw.set_border_width(1)
-		canvas1 = self.animacion()
-		#canvas1.set_size_request(100, 100)
+		#La parte donde estara el grid
+		self.sw = Gtk.ScrolledWindow()
+		container.attach(self.sw, 0, 1, 60, 90)
+		self.sw.set_border_width(1)
+		self.canvas1 = self.animacion()
+		self.sw.add(self.canvas1)
 
-		sw.add(canvas1)
+		#Cosas extras
+		self.buttonPausar = Gtk.Button(label='Pausar')
+		self.buttonPausar.connect('clicked', self.onClick)
+		container.attach_next_to(self.buttonPausar, self.sw, Gtk.PositionType.RIGHT, 5, 5)
 
+		self.buttonCaptura = Gtk.Button(label='TomarCaptura')
+		self.buttonCaptura.connect('clicked', self.imprimirCaptura)
+		container.attach_next_to(self.buttonCaptura, self.buttonPausar, Gtk.PositionType.RIGHT,5,5)
+
+		self.label = Gtk.Label()
+		self.label.set_label('Turno: '+str(self.conteo))
+		container.attach(self.label, 0, 91, 1, 5)
+
+	def imprimirCaptura(self, event):
+		'''
+		Para tomar una foto del estado actual del ScrolledWindow
+		:param event:
+		:return:
+		'''
+		self.figure.savefig(os.getcwd()+'/capturas/hola'+str(self.conteo)+'.png')
 
 	def create_ui_manager(self):
+		'''
+		Para el uso del barmenu
+		:return:
+		'''
 		uimanager = Gtk.UIManager()
-
-		# Throws exception if something went wrong
 		uimanager.add_ui_from_string(UI_INFO)
-
-		# Add the accelerator group to the toplevel window
 		accelgroup = uimanager.get_accel_group()
 		self.add_accel_group(accelgroup)
 		return uimanager
 
+
+
 	def menuArchivo(self, action_group):
+		'''
+		Pestaña 'Archivo'
+		:param action_group:
+		:return:
+		'''
 		action_group.add_actions(
 			[
 				("archivo", None, "Archivo"),
 				("cargarInicio", None, 'Cargar', '<control>N', None, self.cargaEstado),
 				("guardarSimulacion", None, 'Guardar Estado', "<control>G", None, self.guardarEstado),
-				("random",None, 'Configuracoin Random', "<control>R", None, self.random),
+				("random",None, 'Configuracion Random', "<control>R", None, self.random),
 			]
 		)
+
 		action_filequit = Gtk.Action(name="FileQuit", stock_id=Gtk.STOCK_QUIT)
 		action_filequit.connect("activate", self.cerrarVentana)
 		action_group.add_action(action_filequit)
 
 	def menuConfig(self, action_group):
+		'''
+		Pestaña 'Configuracion'
+		:param action_group:
+		:return:
+		'''
 		action_group.add_action(Gtk.Action(name="config", label="Configuracion"))
 
 		action_group.add_radio_actions(
@@ -115,6 +157,11 @@ class ventana(Gtk.Window):
 		action_group.add_action(intervalo)
 
 	def menuAyuda(self, action_group):
+		'''
+		Pestaña 'Ayuda'
+		:param action_group:
+		:return:
+		'''
 		action_group.add_actions(
 			[
 				("help", None, "Ayuda"),
@@ -124,15 +171,48 @@ class ventana(Gtk.Window):
 		)
 
 	def superficie(self, widget, current):
+		'''
+		Para diferenciar cual tipo de fronteras se esta usando
+		:param widget:
+		:param current:
+		:return:
+		'''
 		if self.fronteras == 'Toro':
 			self.fronteras = 'Normal'
 		else:
 			self.fronteras = 'Toro'
 
-	def intervalo(self):
-		pass
+	def intervalo(self,widget):
+		'''
+		Abre una ventana para seleccionar el intervalo de actualizacion (por alguna razon despues de unas
+		cuantas iteraciones se relentiza cuando el intervalo es muy pequeño)
+		:param widget:
+		:return:
+		'''
+		win = Gtk.Window()
+		cosoGrid = Gtk.Grid()
+		win.add(cosoGrid)
+		def aceptar(event):
+			inter = textBox.get_text()
+			self.ani.event_source.interval = float(inter)*1000
+			win.destroy()
+
+		textBox = Gtk.Entry()
+		cosoGrid.add(textBox)
+		aceptarBtn = Gtk.Button(label='Aceptar')
+		aceptarBtn.connect('clicked', aceptar)
+
+		cosoGrid.add(aceptarBtn)
+		win.connect("destroy", Gtk.main_quit)
+		win.show_all()
+		Gtk.main()
 
 	def cargaEstado(self, widget):
+		'''
+		Selecciona un archivo para cargar en el ScrolledWindow
+		:param widget:
+		:return:
+		'''
 		dialog = Gtk.FileChooserDialog(
 			title='Seleccione un estado a cargar', parent=self, action=Gtk.FileChooserAction.OPEN,
 		)
@@ -142,71 +222,83 @@ class ventana(Gtk.Window):
 			Gtk.STOCK_OPEN,
 			Gtk.ResponseType.OK
 		)
-
 		response = dialog.run()
+		self.onClick(response)
+		self.anim_running = False
 		if response == Gtk.ResponseType.OK:
 			self.ruta = dialog.get_filename()
+			self.grid = np.genfromtxt(self.ruta, dtype='i', delimiter=' ', skip_header=1)
+			self.conteo = 1
+			self.onClick(response) #Avanza un solo frame para cargar el estado en el ScrolledWindow
+			self.anim_running = False
 		elif response == Gtk.ResponseType.CANCEL:
 			pass
-
 		dialog.destroy()
 
 	def guardarEstado(self, widget):
 		print('coso guardar')
 
 	def random(self, widget):
-		print('random')
+		#Genera una configuracion random
+		self.grid = self.randomGrid(self.dimensionMatriz)
+		self.onClick(widget)
+		self.anim_running = False
+
 
 	def cerrarVentana(self, widget):
+		#Cierra ventana
 		Gtk.main_quit()
 
 	def acercaDe(self, widget):
-		pass
+		#Abre una ventana
+		acercaDe = Gtk.AboutDialog()
+		acercaDe.set_program_name('El juego de la vida')
+		acercaDe.set_authors(['Jsfgefge/AngelMarroquin'])
+		acercaDe.set_comments('Herramientas usadas:\n'
+			'-Python v3.8.5\n'
+			'-Gtk v3.0\n'
+			'-Matplotlib v3.3.2\n'
+			'-Numpy v1.19.2\n')
+		acercaDe.set_title('uwu')
+		acercaDe.run()
+		acercaDe.destroy()
 
 	def codigoVida(self, widget):
 		pass
 
 	def randomGrid(self,N):
+		#Genera una matriz aleatoria
+		return np.random.choice([1,0], N * N, p=[0.2, 0.8]).reshape(N,N)
 
-		"""returns a grid of NxN random values"""
-		return np.random.choice([1,0], N * N, p=[0.2, 0.8]).reshape(N, N)
+	def onClick(self, event):
+		#Al activarse el evento si esta en pausa se inicia y viceversa
+		#self.anim:running ayuda para controlar esa accion
+		if self.anim_running:
+			self.ani.event_source.stop()
+			self.anim_running = False
+		else:
+			self.ani.event_source.start()
+			self.anim_running = True
+
+	def onStart(self, event):
+		#Ya que al inicio la animacion se inicia sola entonces esta funcion la detiene
+		if self.anim_running == False:
+			self.ani.event_source.stop()
+		else:
+			pass
+
+	def iniConteo(self, event):
+		#Para llevar el conteo de turnos
+		if self.anim_running:
+			self.conteo += 1
+			self.label.set_label('Turno: '+str(self.conteo))
 
 	def animacion(self):
-		figure = mlp.figure.Figure()
-		canvas = FigureCanvas(figure)
+		#Animacion
 		N = self.dimensionMatriz
-		updateInterval = 100
-		data = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-				[1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-				[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-		#grid = self.randomGrid(N)
-		grid = np.array(data)
-
-		'''
-		Anotacion mental:
-		Inicializar el grid con una matriz NxN filleada con ceros y mediante eventos indicar que se llene de forma 
-		random o que se llene con la matriz indicada en la ruta uwu
-		
-		***Pendientes:
-			-Toda la pestaña 'Archivo' y todo lo que implica menos random
-			-Configuracion =>Intervalo
-			-Toda la pestaña 'Ayuda'		
-		'''
-
-		if self.ruta == '':
-			grid = self.randomGrid(N)
-		else:
-			grid = np.zeros((N, N), dtype=int)
-
 		def sumaToro(tabla, N):
-			nTabla = tabla.copy()
+			#Fronteras toroidales
+			nTabla = self.grid
 			for i in range(N):
 				for j in range(N):
 					total = int((tabla[i, (j - 1) % N] + tabla[i, (j + 1) % N] +
@@ -223,7 +315,8 @@ class ventana(Gtk.Window):
 			return nTabla
 
 		def sumaNormal(tabla, N):
-			nTabla = tabla.copy()
+			#Fronteras normales
+			nTabla = self.grid
 			for i in range(0, N):
 				for j in range(0, N):
 					total = 0
@@ -289,121 +382,35 @@ class ventana(Gtk.Window):
 			return nTabla
 
 		def reglas(frame, img, tabla, N):
+			#Funcion para la animacion
 			if self.fronteras == 'Normal':
 				nTabla = sumaNormal(tabla, N)
 			if self.fronteras == 'Toro':
 				nTabla = sumaToro(tabla, N)
-
-			# update data
 			img.set_data(nTabla)
 			tabla[:] = nTabla[:]
 			return img,
-
 		# Basicamente para colorear
 		cmap = colors.ListedColormap(['blue', 'red'])  # Para seleccionar que color queremos
 		bounds = [0, 1, 2]  # Para verificar que valores tienen que ser rojo y cuales azules (0=azul,1=rojo)
 		norm = colors.BoundaryNorm(bounds, cmap.N)  # Para colorear el grid
 		# Generacion del Grid y su animacion
-		ax = figure.subplots()
-		img = ax.imshow(grid, cmap=cmap, norm=norm)
-
-
-		self.ani = animation.FuncAnimation(figure, reglas, fargs=(img, grid, N,),
-										   frames=10,
-										   interval=updateInterval,
+		ax = self.figure.subplots()
+		img = ax.imshow(self.grid, cmap=cmap, norm=norm)
+		self.ani = animation.FuncAnimation(self.figure, reglas, fargs=(img, self.grid, self.dimensionMatriz),
+										   frames=100,
+										   interval=self.intervaloActu,
 										   save_count=1,
 										   repeat=True)
 
-		anim_running = False
+		#Estos eventos se activan al cumplirse la accion entre comillas
+		self.figure.canvas.mpl_connect('button_press_event', self.onClick)
+		self.figure.canvas.mpl_connect('draw_event', self.onStart)
+		self.figure.canvas.mpl_connect('draw_event', self.iniConteo)
 
-		def onClick(event):
-			nonlocal anim_running
-			if anim_running:
-				self.ani.event_source.stop()
-				anim_running = False
-			else:
-				self.ani.event_source.start()
-				anim_running = True
+		return self.canvas
 
-		def onStart(event):
-			if anim_running==False:
-				self.ani.event_source.stop()
-			else:
-				pass
 
-		def iniConteo(event):
-			if anim_running:
-				self.conteo+=1
-				print(self.conteo)
-
-		figure.canvas.mpl_connect('button_press_event', onClick)
-		figure.canvas.mpl_connect('draw_event', onStart)
-		figure.canvas.mpl_connect('draw_event', iniConteo)
-
-		return canvas
-
-class  FileChooserWindow(Gtk.Window):
-	def __init__(self):
-		Gtk.Window.__init__(self, title='Seleccion de estado')
-
-		box = Gtk.Box(spacing=6)
-		self.add(box)
-
-		button1=Gtk.Button(label='Seleccione estado')
-		button1.connect('clicked', self.on_file_clicked)
-		box.add(button1)
-
-		button2 = Gtk.Button(label='Seleccione folder')
-		button2.connect('clicked', self.on_folder_clicked)
-		box.add(button2)
-
-	def on_file_clicked(self, widget):
-		dialog = Gtk.FileChooserDialog(
-			title='Seleccione un estado a cargar', parent=self, action=Gtk.FileChooserAction.OPEN
-		)
-		dialog.add_buttons(
-			Gtk.STOCK_CANCEL,
-			Gtk.ResponseType.CANCEL,
-			Gtk.STOCK_OPEN,
-			Gtk.ResponseType.OK
-		)
-
-		self.add_filters(dialog)
-
-		response = dialog.run()
-		if response == Gtk.ResponseType.OK:
-			print('Open clicked')
-			print('File selected: ', dialog.get_filename())
-		elif response == Gtk.ResponseType.CANCEL:
-			print('Cancel clicked')
-
-		dialog.destroy()
-
-	def add_filters(self, dialog):
-		filter_any = Gtk.FileFilter()
-		filter_any.set_name("Any files")
-		filter_any.add_pattern("*")
-		dialog.add_filter(filter_any)
-
-	def on_folder_clicked(self, widget):
-		dialog = Gtk.FileChooserDialog(
-			title="Please choose a folder",
-			parent=self,
-			action=Gtk.FileChooserAction.SELECT_FOLDER,
-		)
-		dialog.add_buttons(
-			Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-		)
-		dialog.set_default_size(800, 400)
-
-		response = dialog.run()
-		if response == Gtk.ResponseType.OK:
-			print("Select clicked")
-			print("Folder selected: " + dialog.get_filename())
-		elif response == Gtk.ResponseType.CANCEL:
-			print("Cancel clicked")
-
-		dialog.destroy()
 
 
 win = ventana()
